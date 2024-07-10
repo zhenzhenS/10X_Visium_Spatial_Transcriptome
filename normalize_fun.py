294641d97019303abda756c0f2fe63ed8653d810
zhenzhenS <54889893+zhenzhenS@users.noreply.github.com>
2024-07-10 15:46:20 +0300

    normalize_fun.py

--- normalize_fun.py
+++ normalize_fun.py
@@ -1,4 +1,4 @@
-def norm_by_layer(adata):
+def norm_spots(adata):
   for i in np.unique(adata.obs.sample_id.values):
     mean_value=[]
     mean_value_df=[]
@@ -8,3 +8,20 @@ def norm_by_layer(adata):
     all_mean=mean_value_df.mean(axis=0).to_numpy() #all layer mean calculate sample mean
     adata[adata.obs.sample_id == i].X=csr_matrix(adata[adata.obs.sample_id == i].X-all_mean)
   return adata
+
+def norm_layer(adata):
+    # calculate mean gene expr for each sample
+    gene_mean_list = []
+    sample_layer_list = adata.obs.sample_id.unique().tolist()
+    for sample in sample_layer_list:
+        gene_mean_list.append(adata[adata.obs.sample_id == sample].X.mean(axis=0).reshape(-1, 1))
+    # convert to DataFrame
+    gene_mean_df = pd.DataFrame(np.concatenate(gene_mean_list, axis=1), columns=sample_layer_list, index=adata.var_names)
+    gene_mean_df.head()
+    
+    # subtract mean and return adata
+    for sample in sample_layer_list:
+        columns = adata.obs[adata.obs.sample_id == sample].index.to_list()
+        for column in columns:
+            adata[column].X = adata[column].X - gene_mean_df.loc[:, sample].values
+    return adata
